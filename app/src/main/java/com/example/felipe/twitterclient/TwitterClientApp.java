fa5ee0059eaecd26b926dae619bc9b0565374b25
Felipe Gavilan <elmejor_887@hotmail.com>
2016-07-14 19:01:15 -0400

    Configured twitter, fabric, gradle, no action bar.

---
+++ app/src/main/java/com/example/felipe/twitterclient/TwitterClientApp.java
@@ -0,0 +1,21 @@
+package com.example.felipe.twitterclient;
+
+import android.app.Application;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+public class TwitterClientApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initFabric();
+    }
+
+    private void initFabric() {
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(BuildConfig.TWITTER_KEY, BuildConfig.TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+}
