a9b62b342623890c6005de5a34804177994a1288
Felipe Gavilan <elmejor_887@hotmail.com>
2016-07-14 19:19:54 -0400

    Configured Twitter Api

---
+++ app/src/main/java/com/example/felipe/twitterclient/api/CustomTwitterApiClient.java
@@ -0,0 +1,14 @@
+package com.example.felipe.twitterclient.api;
+
+import com.twitter.sdk.android.core.Session;
+import com.twitter.sdk.android.core.TwitterApiClient;
+
+public class CustomTwitterApiClient extends TwitterApiClient {
+    public CustomTwitterApiClient(Session session) {
+        super(session);
+    }
+
+    public TimelineService getTimelineService(){
+        return getService(TimelineService.class);
+    }
+}
