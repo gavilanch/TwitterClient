774034b1734de0086eea497644293147ad83e9b1
Felipe Gavilan <elmejor_887@hotmail.com>
2016-09-04 14:45:13 -0400

    Implemented login logic. Created MainActivity

--- app/src/main/java/com/example/felipe/twitterclient/LoginActivity.java
+++ app/src/main/java/com/example/felipe/twitterclient/LoginActivity.java
@@ -1,13 +1,62 @@
 package com.example.felipe.twitterclient;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+
+import com.example.felipe.twitterclient.main.MainActivity;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.twitterLoginButton)
+    TwitterLoginButton twitterLoginButton;
+    @Bind(R.id.container)
+    RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        if (Twitter.getSessionManager().getActiveSession() != null){
+             navigateToMainScreen();
+        }
+
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                navigateToMainScreen();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                String msgError = String.format(getString(R.string.login_error_message), exception.getMessage());
+                Snackbar.make(container, msgError, Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void navigateToMainScreen() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+
 }
---
+++ app/src/main/java/com/example/felipe/twitterclient/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.felipe.twitterclient.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.felipe.twitterclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
