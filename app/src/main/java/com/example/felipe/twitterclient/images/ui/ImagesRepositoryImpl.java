da58abf0857b825594f4ba32a398bf21ab118cf7
Felipe Gavilan <elmejor_887@hotmail.com>
2016-09-06 19:43:07 -0400

    Created imagesRepositoryImpl

---
+++ app/src/main/java/com/example/felipe/twitterclient/images/ui/ImagesRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.example.felipe.twitterclient.images.ui;
+
+import com.example.felipe.twitterclient.images.ImagesRepository;
+
+/**
+ * Created by Felipe on 06-Sep-16.
+ */
+public class ImagesRepositoryImpl implements ImagesRepository{
+    @Override
+    public void getImages() {
+
+    }
+}
